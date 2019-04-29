1e7d3487228d93927fb8d227d5f58ddb6381c559
Patrick Ndonga <rick.ndonga.com>
2019-04-29 07:37:14 +0300

    Add js script

---
+++ js/script.js
@@ -0,0 +1,20 @@
+function result(){
+  var side1=parseInt(document.getElementById('side1').value);
+  var side2=parseInt(document.getElementById('side2').value);
+  var side3=parseInt(document.getElementById('side3').value);
+  var answer=document.getElementById('answer');
+  if(side1+side2>side3 && side2+side3>side1 && side1+side3>side2) {
+    if (side1==side2&&side1==side3) {
+      answer.textContent='Equilateral: All sides are equal!';
+    }
+    else if(side1===side2||side2===side3||side1===side3){
+      answer.textContent='Isosceles: Two sides are equal!'
+    }
+    else{
+      answer.textContent="Scalene: No sides are equal!";
+    }
+  }
+   else {
+    answer.textContent="Not a triangle!";
+  }
+  }
